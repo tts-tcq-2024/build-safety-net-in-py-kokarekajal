603c25c6eff5e62c0bf99529211acabfe44772e8
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-07-10 18:06:23 +0530

    1_Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,43 +1,16 @@
-def get_soundex_code(c):
-    c = c.upper()
-    mapping = {
-        'B': '1', 'F': '1', 'P': '1', 'V': '1',
-        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
-        'D': '3', 'T': '3',
-        'L': '4',
-        'M': '5', 'N': '5',
-        'R': '6'
-    }
-    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
- 
-def comparison(ch,prev_code):
-    if ch != '0' and ch != prev_code:
-        return ch
-    else:
-        return ""
-def num_map(name,prev_code):
-    soundex = ""
-    for char in name[1:]:
-        ch = get_soundex_code(char)
-        code = comparison(ch,prev_code)
-        if(code != ""):
-            soundex += code
-            prev_code = code
-    return soundex
- 
-def generate_soundex(name):
-    if not name:
-        return ""
-    else:
-        soundex = name[0].upper()
-        prev_code = get_soundex_code(soundex)
-        soundex = soundex + num_map(name,prev_code)
-        if len(soundex) > 4:
-            soundex = soundex[:4]
- 
-    # Start with the first letter (capitalized)
- 
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
- 
-    return soundex
+import unittest
+from Soundex import generate_soundex
+class TestSoundex(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(generate_soundex(""), "")
+    def test_single_character(self):
+        self.assertEqual(generate_soundex("A"), "A000")
+    def test_simple_cases(self):
+        self.assertEqual(generate_soundex("Robert"), "R163")
+        self.assertEqual(generate_soundex("Rupert"), "R163")
+        self.assertEqual(generate_soundex("Rubin"), "R150")
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+        self.assertEqual(generate_soundex("Ashcroft"), "A261")
+        self.assertEqual(generate_soundex("Pfister"), "P236")    
+if __name__ == '__main__':
+    unittest.main()
